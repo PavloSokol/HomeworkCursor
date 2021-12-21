8c43d41f0ff0c74825cb99f697007b70404669bf
Павло Соколюк <pavlosok@i.ua>
2021-12-21 03:03:28 +0200

    fix bugs#2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,14 +9,10 @@ public class Main {
         double w = 5;
         double b = 25;
         double e = 2;
-
-        double result1 = x+y-a / 2*(x+w);
-        double result2 = a * ( x +x )/y-a*3;
-        double result3 = a+b / x-y;
-        double result4 = x-e /2 % e+1;
-
-
-        System.out.println("Результат №1: "+result1+ "\n" +"Результат №2: "+result2+ "\n" +"Результат №3: "+result3+ "\n" +"Результат №4: "+result4+"\n");
-
+        double result1 = x + y - a / 2 * (x + w);
+        double result2 = a * (x + x) / y - a * 3;
+        double result3 = a + b / x - y;
+        double result4 = x - e / 2 % e + 1;
+        System.out.println("Результат №1: " + result1 + "\n" + "Результат №2: " + result2 + "\n" + "Результат №3: " + result3 + "\n" + "Результат №4: " + result4 + "\n");
     }
 }
